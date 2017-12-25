8b83db2d3501a003c208a6133bc69256eb4dec07
Cojamm <idy@marketpress.co.kr>
2017-12-26 00:35:23 +0900

    라이브 코딩

--- js/functional.js
+++ js/functional.js
@@ -1,114 +1,53 @@
-function repeat(fn, count) {
-}
-
-function each(list, fn) {
-  var i = -1, l = list && list.length;
-  while (++i < l) fn(list[i]);
-}
-
-function map(list, mapper) {
-  var new_list = [];
-  for (var i = 0; i < list.length; i++) {
-    new_list[i] = mapper(list[i]);
-  }
-  return new_list;
-}
-
-function map(list, mapper) {
-  var new_list = [];
-  each(list, function(v) {
-    new_list.push(mapper(v));
-  });
-  return new_list;
-}
-
-function filter(list, predi) {
-  var new_list = [];
-  each(list, function(i) {
-    if (predi(list[i])) new_list.push(list[i]);
-  });
-  return new_list;
-}
-
-function key_maker(key) {
-  return function(obj, val) {
-    if (val === undefined) return obj[key];
-    obj[key] = val;
-    return obj;
-  }
-}
-
-Object.assign(window, key_maker(['value']));
-
-function log(v) {
-  return v;
-}
-
-// 1. go 함수를 Promise를 제어할 수 있도록 변경하기
-function bgo(arg, fns, i, fn) {
-  while (fn = fns[++i]) arg = fn(arg);
+function log(arg) {
+  console.log(arg);
   return arg;
 }
 
-function go(arg) {
-  return bgo(arg, arguments, 0);
-}
-
-function pipe() {
-  var fns = arguments;
-  return function(arg) {
-    return bgo(arg, fns, -1);
+function constant(v) {
+  return function() {
+    return v;
   }
 }
 
-function bgo(arg, fns, i, fn) {
-  while (fn = fns[++i]) {
-    if ((arg = fn(arg)) && arg.constructor == Promise)
-      return arg.then(function(arg) { return bgo(arg, fns, i); });
-  }
-  return arg;
+function set_attr(obj, key, val) {
+  obj.setAttribute(key, val);
+  return obj; // <--- 리턴하는식으로
 }
 
-pipe(v=>v+1, v=>v*v, console.log)(1);
-
-
-function length(list) {
-  return list && list.length;
+function sel1(sel) {
+  return document.querySelector(sel);
 }
 
-function repeat(count, fn) {
-  var i = -1;
-  while (++i < count) fn(i)
+function sel(sel) {
+  return document.querySelectorAll(sel);
 }
 
-function add(a, b) {
-  return Promise.resolve(a + b);
+function append(list, child) {
+  return list.push(child), list;
 }
 
-function mult(a, b) {
-  return a * b;
-}
+window.filter = curryr(function(list, predicate) {
+  return reduce(list, function(new_list, val) {
+    return go(val, predicate, evd => evd ? append(new_list, val) : new_list);
+  }, []);
+});
 
-function add_all(list) {
-  var i = 0, count = length(list), memo = list[0];
-  while (++i < count) memo = add(memo, list[i]);
-  return memo;
-}
+window.map = curryr(function(list, mapper) {
+  return reduce(list, function(new_list, val) {
+    return go(val, mapper, evd => append(new_list, evd));
+  }, []);
+});
 
-function mult_all(list) {
-  var i = 0, count = length(list), memo = list[0];
-  while (++i < count) memo = mult(memo, list[i]);
-  return memo;
-}
-
-//  console.log( add_all([1, 2, 3, 4]) );
-//  console.log( mult_all([1, 2, 3, 4]) );
-
+window.each = curryr(function(list, iter) {
+  return reduce(list, function(list, val) {
+    return go(val, iter, constant(list));
+  }, list);
+});
 
 function reduce(list, iter, memo) {
-  var i = 0, count = length(list);
+  var i = 0, l = list.length;
   return function recur(memo) {
-    while (i < count) {
+    while (i < l) {
       if (memo && memo.constructor == Promise) return memo.then(recur);
       memo = iter(memo, list[i++]);
     }
@@ -116,89 +55,33 @@ function reduce(list, iter, memo) {
   } (memo === undefined ? list[i++] : memo);
 }
 
-reduce([1, 2, 3, 4], add, 10).then(function(v) {
-  console.log(v);
-});
-//  console.log( reduce([1, 2, 3, 4], mult) );
-//
-//  console.log( reduce([1, 2, 3, 4], add) );
-
-// 2. 다른 함수 써보기
-function map(list, mapper) {
-  var new_list = [];
-  repeat(length(list), function(i) {
-    new_list.push(mapper(list[i]));
-  });
-  return new_list;
-}
-
-function append(list, child) {
-  return list.push(child), list;
-}
-
-function callr(arg, fn) {
-  return fn(arg);
+function callr(arg, f) {
+  return f(arg);
 }
 
 function pipe() {
   var fns = arguments;
-  return function(arg) { return reduce(fns, callr, arg); }
-}
-
-function pipe() {
-  return arg => reduce(arguments, callr, arg);
+  return function(arg) {
+    return reduce(fns, callr, arg);
+  }
 }
 
 function go() {
   return reduce(arguments, callr);
 }
 
-function map(list, mapper) {
-  return reduce(list, function(new_list, val) {
-    return go(val, mapper, v => append(new_list, v));
-  }, []);
+function curry(fn) {
+  return function(a, b) {
+    return arguments.length == 1 ? function(b) { return fn(a, b); } : fn(a, b);
+  }
 }
 
-function filter(list, predi) {
-  return reduce(list, function(new_list, val) {
-    return go(val, predi, v => v ? append(new_list, val) : new_list);
-  }, []);
+function curryr(fn) {
+  return function(a, b) {
+    return arguments.length == 1 ? function(b) { return fn(b, a); } : fn(a, b);
+  }
 }
 
-map([1,2,3], v=>Promise.resolve(v*2)).then(function(v) {
-  console.log(v);
+window.get = curryr(function(obj, key) {
+  return obj[key];
 });
-
-
-log(filter([1,2,3], v=>v%2));
-filter([1,2,3], v=>Promise.resolve(v%2)).then(log);
-
-go(filter([1,2,3], v=>v%2), log);
-go(filter([1,2,3], v=>Promise.resolve(v%2)), log);
-
-/*
- go(1,
- function(val) {
- console.log(val);
- return val + 2;
- },
- console.log);*/
-
-/*var a = repeat(3, function(i) {
- return i % 2 ? undefined : new Promise(function(rs) {
- setTimeout(function() {
- console.log(i);
- rs();
- }, 1000)
- })
- });
-
- console.log(a);
-
- a.then(console.log);*/
-
-
-
-
-
-
---
+++ js/functional_.js
@@ -0,0 +1,198 @@
+function repeat(fn, count) {
+}
+
+function each(list, fn) {
+  var i = -1, l = list && list.length;
+  while (++i < l) fn(list[i]);
+}
+
+function map(list, mapper) {
+  var new_list = [];
+  for (var i = 0; i < list.length; i++) {
+    new_list[i] = mapper(list[i]);
+  }
+  return new_list;
+}
+
+function map(list, mapper) {
+  var new_list = [];
+  each(list, function(v) {
+    new_list.push(mapper(v));
+  });
+  return new_list;
+}
+
+function filter(list, predi) {
+  var new_list = [];
+  each(list, function(i) {
+    if (predi(list[i])) new_list.push(list[i]);
+  });
+  return new_list;
+}
+
+function key_maker(key) {
+  return function(obj, val) {
+    if (val === undefined) return obj[key];
+    obj[key] = val;
+    return obj;
+  }
+}
+
+Object.assign(window, key_maker(['value']));
+
+function log(v) {
+  return v;
+}
+
+// 1. go 함수를 Promise를 제어할 수 있도록 변경하기
+function bgo(arg, fns, i, fn) {
+  while (fn = fns[++i]) arg = fn(arg);
+  return arg;
+}
+
+function go(arg) {
+  return bgo(arg, arguments, 0);
+}
+
+function pipe() {
+  var fns = arguments;
+  return function(arg) {
+    return bgo(arg, fns, -1);
+  }
+}
+
+function bgo(arg, fns, i, fn) {
+  while (fn = fns[++i]) {
+    if ((arg = fn(arg)) && arg.constructor == Promise)
+      return arg.then(function(arg) { return bgo(arg, fns, i); });
+  }
+  return arg;
+}
+
+pipe(v=>v+1, v=>v*v, console.log)(1);
+
+
+function length(list) {
+  return list && list.length;
+}
+
+function repeat(count, fn) {
+  var i = -1;
+  while (++i < count) fn(i)
+}
+
+function add(a, b) {
+  return Promise.resolve(a + b);
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function add_all(list) {
+  var i = 0, count = length(list), memo = list[0];
+  while (++i < count) memo = add(memo, list[i]);
+  return memo;
+}
+
+function mult_all(list) {
+  var i = 0, count = length(list), memo = list[0];
+  while (++i < count) memo = mult(memo, list[i]);
+  return memo;
+}
+
+//  console.log( add_all([1, 2, 3, 4]) );
+//  console.log( mult_all([1, 2, 3, 4]) );
+
+
+function reduce(list, iter, memo) {
+  var i = 0, count = length(list);
+  return function recur(memo) {
+    while (i < count) {
+      if (memo && memo.constructor == Promise) return memo.then(recur);
+      memo = iter(memo, list[i++]);
+    }
+    return memo;
+  } (memo === undefined ? list[i++] : memo);
+}
+
+reduce([1, 2, 3, 4], add, 10).then(function(v) {
+  console.log(v);
+});
+//  console.log( reduce([1, 2, 3, 4], mult) );
+//
+//  console.log( reduce([1, 2, 3, 4], add) );
+
+// 2. 다른 함수 써보기
+function map(list, mapper) {
+  var new_list = [];
+  repeat(length(list), function(i) {
+    new_list.push(mapper(list[i]));
+  });
+  return new_list;
+}
+
+function append(list, child) {
+  return list.push(child), list;
+}
+
+function callr(arg, fn) {
+  return fn(arg);
+}
+
+function pipe() {
+  var fns = arguments;
+  return function(arg) { return reduce(fns, callr, arg); }
+}
+
+function pipe() {
+  return arg => reduce(arguments, callr, arg);
+}
+
+function go() {
+  return reduce(arguments, callr);
+}
+
+function map(list, mapper) {
+  return reduce(list, function(new_list, val) {
+    return go(val, mapper, v => append(new_list, v));
+  }, []);
+}
+
+function filter(list, predi) {
+  return reduce(list, function(new_list, val) {
+    return go(val, predi, v => v ? append(new_list, val) : new_list);
+  }, []);
+}
+
+map([1,2,3], v=>Promise.resolve(v*2)).then(function(v) {
+  console.log(v);
+});
+
+
+log(filter([1,2,3], v=>v%2));
+filter([1,2,3], v=>Promise.resolve(v%2)).then(log);
+
+go(filter([1,2,3], v=>v%2), log);
+go(filter([1,2,3], v=>Promise.resolve(v%2)), log);
+
+/*
+ go(1,
+ function(val) {
+ console.log(val);
+ return val + 2;
+ },
+ console.log);*/
+
+/*var a = repeat(3, function(i) {
+ return i % 2 ? undefined : new Promise(function(rs) {
+ setTimeout(function() {
+ console.log(i);
+ rs();
+ }, 1000)
+ })
+ });
+
+ console.log(a);
+
+ a.then(console.log);*/
